c6f88682c487d9a836057f05e0283d97fcb37d52
sabe111 <1512670@stud.hs-mannheim.de>
2016-11-09 16:44:07 +0100

    Meilenstein 1  rdy

--- CNC/Interface/get_request.js
+++ CNC/Interface/get_request.js
@@ -1,18 +1,80 @@
-var xhr = new XMLHttpRequest();
+var dataUse = function(data) {
 
-xhr.open('GET', 'http://botnet.artificial.engineering:80/api/Status');
+        // Explizite Methode
+        //data.forEach(function (element, i) {
+        //    console.log(element.ip);
+        // });
 
-xhr.responseType = 'json';
-xhr.setRequestHeader('Content-Type', 'application/json');
-xhr.setRequestHeader('Token', 'my-token-123');
+        //So läuft foreach intern
+        // Array.prototype.forEach = function(callback) {
+        //  ....
+        //  callback(val, index);
+        // };
 
-xhr.onload = function() {
+        var code = data.map((val, index) => {
+                //Explizite Methode, jedes Attribut einzeln aufschreiben
+                //return '<tr><td>'+val.ip +'</td><td>' + val.workload + '</td></tr>';
 
-    var data = xhr.response;
-    if (data !== null) {
-        console.log(data); // Parsed JSON object
-    }
+                var wl;
+                if(val.workload===0){
+                   wl =    '<label class="switch">'+
+                           '<input type="checkbox">'+
+                           '<div class="slider round"></div>'+
+                           '</label>';
+                } else{
+                 wl =      '<label class="switch">'+
+                           '<input type="checkbox" checked>'+
+                           '<div class="slider round"></div>'+
+                           '</label>';
+                }
+
+
+                //Einzelne Attribute des Array-Objekts werden zusammengefügt mit join
+                return '<tr><td>' + Object.values(val).join('</td><td>') + '</td>'+'<td>'+wl+'</td></tr>';
+
+            }).join('\n');
+
+
+console.log(code);
+
+return code;
 
 };
 
-xhr.send(null);
+        /*    var myarr = data.filter(function(val, index) {
+              return val.ip !== '127.0.0.1';
+            }).map(function(val, index) {
+              return '<td>' + val.ip + '</td>...<td>val.workload...</td>';
+            });
+
+            var html_code = '<tr>' + myarr.join('</tr><tr>') + '</tr>';
+        */
+
+
+
+
+
+
+
+        var xhr = new XMLHttpRequest();
+
+        xhr.open('GET', 'http://botnet.artificial.engineering:80/api/Status');
+
+        xhr.responseType = 'json';
+        xhr.setRequestHeader('Content-Type', 'application/json');
+        xhr.setRequestHeader('Token', 'my-token-123');
+
+        xhr.onload = function() {
+
+            var data = xhr.response;
+            if (data !== null) {
+                console.log(data); // Parsed JSON object
+              var element = document.querySelector('#status tbody');
+              element.innerHTML =   dataUse(data);
+
+
+            }
+
+        };
+
+        xhr.send(null);
